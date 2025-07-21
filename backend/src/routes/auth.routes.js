2e85d519b67e4df2bc44d787796cfa5ef916b059
core.editor <soumen@example.com>
2025-07-21 23:32:15 +0530

    feat: add onboarding functionality and protect routes with JWT authentication

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { login, logout, onboard, signup } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login); 
 router.post('/logout', logout);
+router.post('/onboarding', protectRoute, onboard);
+
+router.get('/me', protectRoute, (req, res) => {
+    res.status(200).json({ message: 'This is a protected route', user: req.user });
+});
 
 export default router;
