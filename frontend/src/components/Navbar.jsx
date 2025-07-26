7978154c160c4b18f8671407bda2d3b881eddb59
core.editor <soumen@example.com>
2025-07-26 23:31:13 +0530

    feat: add layout, sidebar, navbar, and theme selector components; implement logout functionality and enhance user authentication flow

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import useAuthUser from '../hooks/useAuthUser';
+import { Link, useLocation } from 'react-router';
+import { BellIcon, LogOutIcon, LucideShipWheel } from 'lucide-react';
+import ThemeSelector from './ThemeSelector.jsx';
+import useLogout from '../hooks/useLogout.js';
+
+const Navbar = () => {
+    const {authUser} = useAuthUser();
+    const location = useLocation();
+    const isChatPage = location.pathname?.startsWith('/chat');
+
+    // const queryClient = useQueryClient();
+
+    // const {mutate:logoutMutation} = useMutation({
+    //     mutationFn: logout,
+    //     onSuccess: () => {
+    //         queryClient.invalidateQueries({
+    //             queryKey: ['authUser'],
+    //         });
+    //     },
+    // });
+    const {logoutMutation} = useLogout()
+
+    return (
+        <nav className='bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center '>
+            <div className='container mx-auto px-4 sm:px-6 lg:px-8'>
+                <div className='flex items-center justify-end w-full'>
+                    {/* Logo - Only in the chat page */}
+                    {isChatPage && (
+                        <div className='pl-5'>
+                            <Link to='/' className='flex items-center gap-2.5'>
+                                <LucideShipWheel className='size-9 text-primary' />
+                                <span className='text-2xl font-bold font-mono bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent tracking-wider'>ChitChat</span>
+                            </Link>
+                        </div>
+                    )}
+
+                    <div className='flex items-center gap-3 sm:gap-4'>
+                        <Link to='/notifications'>
+                            <button className='btn btn-ghost btn-circle'>
+                                <BellIcon className='size-6 text-base-content opacity-70' />
+                            </button>
+                        </Link>
+                    </div>
+
+                    <ThemeSelector />
+
+                    {/* User Profile Section */}
+                    <div className='avatar'>
+                        <div className='w-8 rounded-full'>
+                            <img src={authUser?.profilePic} alt={authUser?.name} rel='noreferrer' />
+                        </div>
+                    </div>
+
+                    {/* Logout Button */}
+                    <div>
+                        <button className='btn btn-ghost btn-circle' onClick={logoutMutation}>
+                            <LogOutIcon className='size-6 text-base-content opacity-70' />
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </nav>
+    )
+}
+
+export default Navbar
